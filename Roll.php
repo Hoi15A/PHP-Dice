b312b8f977838dbf8c01970466f71ace8f03b16f
SeverinFuerbringer <severin@protonmail.ch>
2016-01-24 23:48:28 +0100

    Made the whole program less messier and more re-usable.

--- Roll.php
+++ Roll.php
@@ -1,181 +1,78 @@
 <!DOCTYPE html>
 <html>
-<head>
-    
-    <link href="FancyPants.css" type="text/css" rel="stylesheet">
-    <link href='https://fonts.googleapis.com/css?family=Arvo' rel='stylesheet' type='text/css'>
-    <title>The Dice has Spoken</title>
+	<head>
 
-</head>
-<body>
+		<link href="FancyPants.css" type="text/css" rel="stylesheet">
+		<link href='https://fonts.googleapis.com/css?family=Arvo' rel='stylesheet' type='text/css'>
+		<title>The Dice has Spoken</title>
 
-<?php
-echo "<h1>PHP Dice</h1>";
+	</head>
+	<body>
 
-$rngNum1 = mt_rand(1,6);
-$rngNum2 = mt_rand(1,6);
-$rngNum3 = mt_rand(1,6);
-$rngTotal = 0;
+		<?php
 
-if($_POST["diceCount"] == 1){
+		/**
+		 * @return array Array with random integers (range 1-6)
+		 * @param Integer $amount
+		 */
+		function generateRandNumbers($amount) {
+			$ret = array();
 
-    switch($rngNum1){
-        
-        case 1:
-        echo '<img src="DiceImages/One.png" alt="One" height="200" width="200">';
-        break;
-        case 2:
-        echo '<img src="DiceImages/Two.png" alt="Two" height="200" width="200">';
-        break;
-        case 3:
-        echo '<img src="DiceImages/Three.png" alt="Three" height="200" width="200">';
-        break;
-        case 4:
-        echo '<img src="DiceImages/Four.png" alt="Four" height="200" width="200">';
-        break;
-        case 5:
-        echo '<img src="DiceImages/Five.png" alt="Five" height="200" width="200">';
-        break;
-        case 6:
-        echo '<img src="DiceImages/Six.png" alt="Six" height="200" width="200">';
-        break;
-    }
+			for ($i = 0; $i < $amount; $i++) {
+				array_push($ret, mt_rand(1, 6));
+			}
+			return $ret;
+		}
 
-}
-elseif($_POST["diceCount"] == 2){
+		/**
+		 * Displays a dice with its $number
+		 * @param Integer $number
+		 */
+		function outputDice($number) {
+			switch ($number) {
+				case 1:
+					echo '<img src="DiceImages/One.png" alt="One" height="200" width="200">';
+					break;
+				case 2:
+					echo '<img src="DiceImages/Two.png" alt="Two" height="200" width="200">';
+					break;
+				case 3:
+					echo '<img src="DiceImages/Three.png" alt="Three" height="200" width="200">';
+					break;
+				case 4:
+					echo '<img src="DiceImages/Four.png" alt="Four" height="200" width="200">';
+					break;
+				case 5:
+					echo '<img src="DiceImages/Five.png" alt="Five" height="200" width="200">';
+					break;
+				case 6:
+					echo '<img src="DiceImages/Six.png" alt="Six" height="200" width="200">';
+					break;
+			}
+		}
 
-    switch($rngNum1){
-        
-        case 1:
-        echo '<img src="DiceImages/One.png" alt="One" height="200" width="200">';
-        break;
-        case 2:
-        echo '<img src="DiceImages/Two.png" alt="Two" height="200" width="200">';
-        break;
-        case 3:
-        echo '<img src="DiceImages/Three.png" alt="Three" height="200" width="200">';
-        break;
-        case 4:
-        echo '<img src="DiceImages/Four.png" alt="Four" height="200" width="200">';
-        break;
-        case 5:
-        echo '<img src="DiceImages/Five.png" alt="Five" height="200" width="200">';
-        break;
-        case 6:
-        echo '<img src="DiceImages/Six.png" alt="Six" height="200" width="200">';
-        break; 
-    }
+		echo "<h1>PHP Dice</h1>";
 
-    switch($rngNum2){
-        
-        case 1:
-        echo '<img src="DiceImages/One.png" alt="One" height="200" width="200">';
-        break;
-        case 2:
-        echo '<img src="DiceImages/Two.png" alt="Two" height="200" width="200">';
-        break;
-        case 3:
-        echo '<img src="DiceImages/Three.png" alt="Three" height="200" width="200">';
-        break;
-        case 4:
-        echo '<img src="DiceImages/Four.png" alt="Four" height="200" width="200">';
-        break;
-        case 5:
-        echo '<img src="DiceImages/Five.png" alt="Five" height="200" width="200">';
-        break;
-        case 6:
-        echo '<img src="DiceImages/Six.png" alt="Six" height="200" width="200">';
-        break; 
-    }
+		// Generates an array with random numbers
+		$rndNumArray = generateRandNumbers($_POST["diceCount"]);
+		$rndTotal = 0;
+		// Maximum dice allowed
+		$maxDice = 3;
 
-    $rngTotal = $rngNum1 + $rngNum2;
+		if (count($rndNumArray) <= $maxDice || count($rndNumArray) < 1) {
+			for ($i = 0; $i < count($rndNumArray); $i++) {
+				outputDice($rndNumArray[$i]);
+				$rndTotal += $rndNumArray[$i];
+			}
+			echo "<h3>Total: $rndTotal";
+		} else {
 
-    echo "<h3>Total: $rngTotal";
+			echo '<div id="fail">ERROR:<br/>No valid dice count entered!</div>';
+		}
 
-}
-elseif($_POST["diceCount"] == 3){
-
-    switch($rngNum1){
-        
-        case 1:
-        echo '<img src="DiceImages/One.png" alt="One" height="200" width="200">';
-        break;
-        case 2:
-        echo '<img src="DiceImages/Two.png" alt="Two" height="200" width="200">';
-        break;
-        case 3:
-        echo '<img src="DiceImages/Three.png" alt="Three" height="200" width="200">';
-        break;
-        case 4:
-        echo '<img src="DiceImages/Four.png" alt="Four" height="200" width="200">';
-        break;
-        case 5:
-        echo '<img src="DiceImages/Five.png" alt="Five" height="200" width="200">';
-        break;
-        case 6:
-        echo '<img src="DiceImages/Six.png" alt="Six" height="200" width="200">';
-        break; 
-    }
-
-    switch($rngNum2){
-        
-        case 1:
-        echo '<img src="DiceImages/One.png" alt="One" height="200" width="200">';
-        break;
-        case 2:
-        echo '<img src="DiceImages/Two.png" alt="Two" height="200" width="200">';
-        break;
-        case 3:
-        echo '<img src="DiceImages/Three.png" alt="Three" height="200" width="200">';
-        break;
-        case 4:
-        echo '<img src="DiceImages/Four.png" alt="Four" height="200" width="200">';
-        break;
-        case 5:
-        echo '<img src="DiceImages/Five.png" alt="Five" height="200" width="200">';
-        break;
-        case 6:
-        echo '<img src="DiceImages/Six.png" alt="Six" height="200" width="200">';
-        break; 
-    }
-
-    switch($rngNum3){
-        
-        case 1:
-        echo '<img src="DiceImages/One.png" alt="One" height="200" width="200">';
-        break;
-        case 2:
-        echo '<img src="DiceImages/Two.png" alt="Two" height="200" width="200">';
-        break;
-        case 3:
-        echo '<img src="DiceImages/Three.png" alt="Three" height="200" width="200">';
-        break;
-        case 4:
-        echo '<img src="DiceImages/Four.png" alt="Four" height="200" width="200">';
-        break;
-        case 5:
-        echo '<img src="DiceImages/Five.png" alt="Five" height="200" width="200">';
-        break;
-        case 6:
-        echo '<img src="DiceImages/Six.png" alt="Six" height="200" width="200">';
-        break; 
-    }
-
-    $rngTotal = $rngNum1 + $rngNum2 + $rngNum3;
-
-    echo "<h3>Total: $rngTotal";
-
-}
-else{
-
-    echo '<div id="fail">ERROR:<br/>No valid dice count entered!</div>';
-
-}
-
-echo '<h3><a href="index.php">Back to Start</a></h3>';
-
-?>
+		echo '<h3><a href="index.php">Back to Start</a></h3>';
+		?>
 
 
-</body>
+	</body>
 </html>
