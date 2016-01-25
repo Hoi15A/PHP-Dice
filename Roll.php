60c445c68809333a3d57190c63db1e9718fc5d5d
SeverinFuerbringer <severin@protonmail.ch>
2016-01-25 20:47:18 +0100

    Removed unnecessary switch-case.

--- Roll.php
+++ Roll.php
@@ -29,26 +29,11 @@
 		 * @param Integer $number
 		 */
 		function outputDice($number) {
-			switch ($number) {
-				case 1:
-					echo '<img src="DiceImages/One.png" alt="One" height="200" width="200">';
-					break;
-				case 2:
-					echo '<img src="DiceImages/Two.png" alt="Two" height="200" width="200">';
-					break;
-				case 3:
-					echo '<img src="DiceImages/Three.png" alt="Three" height="200" width="200">';
-					break;
-				case 4:
-					echo '<img src="DiceImages/Four.png" alt="Four" height="200" width="200">';
-					break;
-				case 5:
-					echo '<img src="DiceImages/Five.png" alt="Five" height="200" width="200">';
-					break;
-				case 6:
-					echo '<img src="DiceImages/Six.png" alt="Six" height="200" width="200">';
-					break;
-			}
+			$dicePictures = array("One.png", "Two.png", "Three.png",
+				"Four.png", "Five.png", "Six.png");
+
+			echo '<img src="DiceImages/' . $dicePictures[$number - 1] .
+				 '" alt="Six" height="200" width="200">';
 		}
 
 		echo "<h1>PHP Dice</h1>";
