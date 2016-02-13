2837194f1e0faf60170e8b58d2f04e70b3bcf3ef
SeverinFuerbringer <severin@protonmail.ch>
2016-02-13 20:48:12 +0100

    Removed quotation mark.

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 
 <?php
 echo <<<INDEX
-	<h1>PHP Dice</h1>"
+	<h1>PHP Dice</h1>
 	<form action="Roll.php" method="post">
 	<h2>Amount of Dice: </h2><input type="number" name="diceCount" min="1" max="3" step="1" value="2">
 	<input type="submit" value="Roll the Dice">
