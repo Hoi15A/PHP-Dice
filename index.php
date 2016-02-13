443da867a52ae3179b25f388d461dc9c97eaa8cc
SeverinFuerbringer <severin@protonmail.ch>
2016-02-13 20:44:47 +0100

    Replaced multiple echo commands with a single echo using Heredoc.

--- index.php
+++ index.php
@@ -10,12 +10,13 @@
 <body>
 
 <?php
-echo "<h1>PHP Dice</h1>";
-
-echo '<form action="Roll.php" method="post">';
-echo '<h2>Amount of Dice: </h2><input type="number" name="diceCount" min="1" max="3" step="1" value="2">';
-echo '<input type="submit" value="Roll the Dice">';
-echo '</form>';
+echo <<<INDEX
+	<h1>PHP Dice</h1>"
+	<form action="Roll.php" method="post">
+	<h2>Amount of Dice: </h2><input type="number" name="diceCount" min="1" max="3" step="1" value="2">
+	<input type="submit" value="Roll the Dice">
+	</form>
+INDEX;
 ?>
 
 </body>
